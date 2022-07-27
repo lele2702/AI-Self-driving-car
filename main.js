064e73e45a172faffa553c2c420e5d3010e5da7c
lele2702 <gioele.iannelli1996@gmail.com>
2022-07-27 23:01:26 +0200

    first implementation of canvas and a primitive car

---
+++ main.js
@@ -0,0 +1,12 @@
+/*
+* Created by Gioele Iannelli
+* method to center the canvas 
+* and create the road
+*/
+const canvas=document.getElementById("myCanvas");
+canvas.height=window.innerHeight;
+canvas.width=200;
+
+const ctx = canvas.getContext("2d");
+const car = new Car(100,100,30,50);
+car.draw(ctx);
